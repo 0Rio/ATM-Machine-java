13c7a5b83547337f85a2a1ec48a227f69be8379b
dan <dan@R-DAN>
2022-04-17 01:35:45 +0530

    Added checking through linear search

---
+++ ATM Machine/src/main/main.java
@@ -0,0 +1,72 @@
+package main;
+
+import java.util.Scanner;
+
+public class main 
+{
+	 private static int[] acc = {8080897, 3293724, 8920910, 1294733, 9878677};
+	final private static int[] pass = {2087, 7068, 9075, 3719, 3874};
+	static int accNo = 0, pas = 0;;
+	
+	static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args)
+	{
+		input();
+	}
+	
+	
+	private static void input() 
+	{
+		
+		System.out.print("Enter your Account Number: ");
+		accNo = in.nextInt();
+		System.out.print("Enter your Pin Number: ");
+		pas = in.nextInt();
+		Invalid();
+	}
+
+
+	
+
+
+	private static void Invalid() 
+	{
+		int Alength = Integer.toString(accNo).length();
+		int paLength = Integer.toString(pas).length();
+		if(!(paLength == 4) || !(Alength == 7) )
+		{
+			System.out.println("Invalid Account Number or Pin");
+			input();
+		}
+		else
+		{
+			if(!check())
+			{
+				System.out.println("Invalid Account Number or Pin");
+				input();
+			}
+		}
+		
+	}
+	
+private static Boolean check() 
+	{
+	
+	for (int i = 0, length = acc.length; i < length; i++)
+	{
+		if(acc[i] == accNo && pass[i] == pas)
+		{
+			
+		}
+	}
+		return false;	
+	}
+}
+
+
+
+
+
+
+
